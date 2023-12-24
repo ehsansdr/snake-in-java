3beaa2fd79216a5729887420080966c69e433189
ehsansdr <sehsan251@gmail.com>
2023-12-24 10:11:49 +0330

    first getting experience

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //hello son I am ehsan and this project is gunna be snake project
+        //and upload as CV
+        //soo set back and enjoy code and comments to inform you
+
+
+    }
+}

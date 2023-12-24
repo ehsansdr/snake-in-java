9b2db370a3c6c8d07d22d0f0618f8b6862a01741
ehsansdr <sehsan251@gmail.com>
2023-12-24 10:14:21 +0330

    fff

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //hello son I am ehsan and this project is gunna be snake project
         //and upload as CV
-        //soo set back and enjoy code and comments to inform you
+        //soo set back and enjoy code and comments to inform you ffff
 
 
     }

f4441cb908108e7b3db6ca0a17e095e5760ae768
ehsansdr <sehsan251@gmail.com>
2023-12-25 17:55:39 +0330

    grid created

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,10 +2,11 @@ import javax.swing.*;
 
 public class GameFrame extends JFrame {
     GameFrame(){
-        this.setResizable(false);
+        this.setResizable(true);
         this.setLocation(1200,150);
         this.setTitle("snake");
-        this.pack();
+        this.add(new GamePanel());
+        this.pack();/** it is important to have this after of add() methods */
         this.setVisible(true);
         System.out.println("GameFrame : done!!!");
     }
---
+++ src/GamePanel.java
@@ -0,0 +1,70 @@
+import lombok.Data;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+@Data
+public class GamePanel extends JPanel implements ActionListener {
+    //********************** DEFAULT INFO ********************
+    boolean running = true;
+    boolean gridMode = true;//if you want to have grid like separated square
+    Color GridColor = new Color(0x797979);
+    int UNIT_SIZE = 30;
+    final int HEADER_HEIGHT = 50;
+    Timer timer;
+
+
+    //************************* PANEL ************************
+    final int PANEL_WIDTH = 600 + 1;
+    final int PANEL_HEIGHT = 600 + HEADER_HEIGHT + 1;//+1  is because of difference  of line of grid
+    Color panelColor = new Color(0x161259);
+
+
+    //************************* FIELD ************************
+    //it is not actually the extra panel that we want to add just want to
+    //have separation between header and game map
+    final int FIELD_WIDTH = PANEL_WIDTH - 1;//-1 is because of difference of  line of grid
+    final int FIELD_HEIGHT = PANEL_HEIGHT - HEADER_HEIGHT;
+
+    //********************** SNAKE BODY **********************
+
+    //************************* APPLE ************************
+    int appleEaten = 0;
+    int XApple ;
+    int YApple ;
+    Color appleColor = new Color(0x860232);
+
+    // ********************** DEFAULT INFO ********************
+    int GAME_UNIT =  (FIELD_WIDTH * FIELD_HEIGHT) / UNIT_SIZE;
+
+    GamePanel(){
+        this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
+        this.setFocusable(true);
+        this.setBackground(panelColor);
+        this.setVisible(true);
+        this.addKeyListener(new MoveSnake());//we add the move and keyListeners in panel not frame
+        System.out.println("GamePanel : done!!!");
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        if(gridMode) {
+            for (int i = 0; i <= FIELD_WIDTH / UNIT_SIZE; i++) {
+                //for horizontal
+                g.setColor(GridColor);
+                g.drawLine(0, (i * UNIT_SIZE) + HEADER_HEIGHT, FIELD_WIDTH, (i * UNIT_SIZE) + HEADER_HEIGHT);
+
+                //for vertical
+                g.setColor(GridColor);
+                g.drawLine(i * UNIT_SIZE, HEADER_HEIGHT, i * UNIT_SIZE, PANEL_HEIGHT);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
